a50790e3e7ff95a320ee17487100137fbd20b827
codeWithGitHUb <101158595+Tayade1999@users.noreply.github.com>
2023-05-11 16:10:55 +0530

    Add files via upload

---
+++ Functions/function sum/src/function sum.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : function.c
+ Author      : Rajkumar
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+
+void sum(int, int);
+
+
+int main() {
+
+	/* Objective: WAP to...... */
+	puts("My program starts... ");
+
+	void sum(2, 3);
+
+	puts("...some dummy code...");
+	puts("More code...");
+
+	sum(10, 90);
+
+	puts("My program continues with a few more functionalities.... ");
+
+	sum(11, 21);
+
+	sum(3, 9);
+
+	return 0;
+}
